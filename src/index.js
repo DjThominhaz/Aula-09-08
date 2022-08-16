b3956caf37baad2276601d94c259f27a465be716
Thomaz Erick Assis dos Santos <thomaz_erick@hotmail.com>
2022-08-16 09:44:26 -0300

    Aula 16/08

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Comp7 from './Componentes/Comp7';
-
+import CompoRend from '../src/Componentes/CompoRend'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <CompoRend/>
   </React.StrictMode>
 );
